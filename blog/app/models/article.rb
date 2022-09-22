bd58a9bf0283f60eb3747f0731e964c2a8a132ac
EdgardoAcosta <acostaleale@gmail.com>
2022-09-21 20:47:03 -0500

    Added user to new article crate

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,9 +1,10 @@
 class Article < ApplicationRecord
+    
+    belongs_to :user
 
     validates :title, presence: true , length: { minimum: 2, maximum: 100 }
 
     validates :description, presence: true, length: { minimum: 5, maximum: 300 }
 
-    belongs_to :user
 
 end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_045309) do
+ActiveRecord::Schema.define(version: 2022_09_21_202728) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
